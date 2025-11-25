d398e78b921350eb4206e5386e7e9bde84c45104
consultoria-panovo <consultoria@panovo.mx>
2025-11-25 17:10:00 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+{
+  "name": "costeo-panovo",
+  "version": "1.0.0",
+  "description": "API para consultar BOMs en Azure SQL",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "mssql": "^9.1.1"
+  }
+}
